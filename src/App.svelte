7825fcd4394e354f34273ad324f402f64e12f14b
David E Luna M <davidlunamontilla@gmail.com>
2022-05-05 07:29:55 -0500

    Se coloca el nombre de la aplicación en el título, además del crédito de quien lo escribió

--- src/App.svelte
+++ src/App.svelte
@@ -5,3 +5,11 @@
 <main>
 	<Reloj />
 </main>
+
+<footer class="footer">
+	&copy; 2022 - &nbsp;
+	Coded by &nbsp;
+	<a href="https://github.com/dlunamontilla/" target="_blank">
+		David E Luna M
+	</a>
+</footer>
